1d850965023a657dda39124182acbafabd0b0910
oldseven <oldseven@oldseven.com>
2019-08-23 16:15:54 +0800

    优化

--- beijing28.py
+++ beijing28.py
@@ -5,17 +5,11 @@ def main(start, end, step):
     result = {}
     arr = []
     for i in combinations([i for i in range(start, end)], step):
-        arr.append(list(i))
+        arr.append(sum(i) % 10)
 
     for i in combinations(arr, 3):
-        total = 0
-        for j in i:
-            total += sum(j) % 10
-
-        if result.get(total):
-            result[total] += 1
-        else:
-            result[total] = 1
+        total = sum(i) 
+        result[total] = result[total] + 1 if result.get(total) else 1
     
     Sum = sum(result.values())
     for i in range(0, 28):
